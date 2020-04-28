a13bd01a6bca0ebafd2af5ad9bf1d665c15b19ab
Muzi <muzi@email.com>
2020-04-28 19:51:42 +0800

    lodash-chunk

--- lodash/me7426-lodash.js
+++ lodash/me7426-lodash.js
@@ -2,4 +2,15 @@ var me7426 = {
 	isNull: function (val) {
 		return val === null
 	},
+
+	chunk: function (ary, size) {
+		let result = [];
+		times = Math.floor(ary.length / size);
+
+		for (let i = 0; i < ary.length; i+=3) {
+			result.push(ary.slice(i, i + 3))
+		}
+
+		return result;
+	},
 }
